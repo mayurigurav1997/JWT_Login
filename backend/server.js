cbdee1bf9cd41b0c25e92c3e8149b4a978741113
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-06-28 18:44:59 +0530

    dotenv used so we can use environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv'
 dotenv.config()
-const port = 5000;
+const port = process.env.PORT || 5000;
+
 const app = express();
 
 app.get('/', (req, res) => res.send("Server is Ready"))
