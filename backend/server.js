f246ddecaf30ce127a12d5e7d3a8a4e44ae15209
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-06-29 20:27:47 +0530

    generate token

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ const port = process.env.PORT || 5000;
 import userRoutes from './routes/userRoutes.js'
 
 connectDB();
+
 const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/users', userRoutes);
 
